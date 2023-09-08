a5ed45fc23c5fcd6ffc1678ca053fe2b3d800a7c
hoshogi <hoshogi@gmail.com>
2023-09-08 23:09:07 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/hoshogi/onlyonepick/OnlyOnePickApplication.java
@@ -0,0 +1,13 @@
+package com.hoshogi.onlyonepick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlyOnePickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlyOnePickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hoshogi/onlyonepick/OnlyOnePickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoshogi.onlyonepick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlyOnePickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
