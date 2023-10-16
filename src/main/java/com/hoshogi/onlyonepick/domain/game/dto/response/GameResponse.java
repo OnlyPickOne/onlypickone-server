b16cdfc599719dd4f13ed92a265aec173341a1c4
hoshogi <hoshogi@gmail.com>
2023-10-17 00:41:11 +0900

    feat: 게임 리스트 조회 기능 수정
    - response dto 수정

--- src/main/java/com/hoshogi/onlyonepick/domain/game/dto/response/GameResponse.java
+++ src/main/java/com/hoshogi/onlyonepick/domain/game/dto/response/GameResponse.java
@@ -3,6 +3,7 @@ package com.hoshogi.onlyonepick.domain.game.dto.response;
 import com.hoshogi.onlyonepick.domain.game.entity.Game;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
@@ -11,20 +12,22 @@ public class GameResponse {
     private Long gameId;
     private String title;
     private String description;
-    private Long viewCount;
     private Long playCount;
+    private Long likeCount;
     private Long itemCount;
     private Long reportCount;
+    private LocalDateTime createdAt;
     private List<String> imageUrls;
 
     public GameResponse(Game game, List<String> imageUrls) {
         this.gameId = game.getGameId();
         this.title = game.getTitle();
         this.description = game.getDescription();
-        this.viewCount = game.getViewCount();
         this.playCount = game.getPlayCount();
+        this.likeCount = game.getLikeCount();
         this.itemCount = game.getItemCount();
         this.reportCount = game.getReportCount();
+        this.createdAt = game.getCreatedAt();
         this.imageUrls = imageUrls;
     }
 }
