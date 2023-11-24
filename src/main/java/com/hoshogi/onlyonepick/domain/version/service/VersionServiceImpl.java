54760f17150c13b57be28ad9dae6bc641d6649e8
hoshogi <hoshogi@gmail.com>
2023-11-25 01:12:19 +0900

    feat: 버전 저장 기능 수정
    - 저장 가능 권한 설정

--- src/main/java/com/hoshogi/onlyonepick/domain/version/service/VersionServiceImpl.java
+++ src/main/java/com/hoshogi/onlyonepick/domain/version/service/VersionServiceImpl.java
@@ -1,11 +1,14 @@
 package com.hoshogi.onlyonepick.domain.version.service;
 
+import com.hoshogi.onlyonepick.domain.member.service.MemberService;
 import com.hoshogi.onlyonepick.domain.version.dto.request.VersionRequest;
 import com.hoshogi.onlyonepick.domain.version.dto.response.VersionResponse;
 import com.hoshogi.onlyonepick.domain.version.entity.Version;
 import com.hoshogi.onlyonepick.domain.version.respository.VersionRepository;
 import com.hoshogi.onlyonepick.global.error.ErrorCode;
 import com.hoshogi.onlyonepick.global.error.exception.BadRequestException;
+import com.hoshogi.onlyonepick.global.error.exception.ForbiddenException;
+import com.hoshogi.onlyonepick.global.util.SecurityUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,18 +18,22 @@ import org.springframework.transaction.annotation.Transactional;
 public class VersionServiceImpl implements VersionService {
 
     private final VersionRepository versionRepository;
-
+    private final MemberService memberService;
+    
     @Override
     @Transactional(readOnly = true)
     public VersionResponse findVersion() {
         Version version = versionRepository.findTopByOrderByCreatedAtDesc()
                 .orElseThrow(() -> new BadRequestException(ErrorCode.VERSION_NOT_FOUND));
-        return VersionResponse.from(version);
+        return VersionResponse.of(version);
     }
 
     @Override
     @Transactional
     public void saveVersion(VersionRequest request) {
+        if (!memberService.findById(SecurityUtil.getCurrentMemberId()).isAdmin()) {
+            throw new ForbiddenException(ErrorCode.FORBIDDEN_USER);
+        }
         versionRepository.save(request.toEntity());
     }
 }
